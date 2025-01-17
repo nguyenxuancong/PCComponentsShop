8b2c0869f89090d014777ccab5621591c3b19204
nguyenxuancong <chauhoa2017.com@gmail.com>
2025-01-17 15:48:13 +0700

    added addresses, users, customers, employees, enum{role} to files

--- WebsiteSellComputerComponent/src/main/java/com/base/entity/Users.java
+++ WebsiteSellComputerComponent/src/main/java/com/base/entity/Users.java
@@ -1,7 +1,9 @@
 package com.base.entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -55,6 +58,9 @@ public class Users {
 	@JoinColumn(name = "employee_id") // cột FK id của nhân viên quan hệ 1 - 1 với user
 	@OneToOne(fetch = FetchType.LAZY)
 	private Employees employee;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Addresses> addresses;
 	
 
 }
